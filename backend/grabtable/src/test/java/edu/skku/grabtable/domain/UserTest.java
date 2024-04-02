5b6b1c89e37acdd651fb404571935762cebe5c48
dmong <dt2980_hee@naver.com>
2024-04-02 17:49:54 +0900

    Test(be): 유저 테스트 템플릿 수정

--- backend/grabtable/src/test/java/edu/skku/grabtable/domain/UserTest.java
+++ backend/grabtable/src/test/java/edu/skku/grabtable/domain/UserTest.java
@@ -1,27 +1,33 @@
 package edu.skku.grabtable.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import jakarta.persistence.EntityManager;
+import edu.skku.grabtable.config.JpaAuditingConfig;
+import edu.skku.grabtable.repository.UserRepository;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Transactional
+@Import(JpaAuditingConfig.class)
 class UserTest {
 
     @Autowired
-    EntityManager em;
+    UserRepository userRepository;
 
     @Test
-    void insertUser(){
-        User user = new User();
-        user.setUsername("od");
-        user.setPassword("1234");
+    void insertUser() {
+        User user = User.builder()
+                .username("od")
+                .password("1234")
+                .email("sesdflk@gmail.com")
+                .phone("01021213212")
+                .build();
+        userRepository.save(user);
 
-        em.persist(user);
+        List<User> result = userRepository.findAll();
+        Assertions.assertThat(result.size()).isEqualTo(1);
     }
 
 

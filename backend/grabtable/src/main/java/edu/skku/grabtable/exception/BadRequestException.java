52b00f0556f86ba831520341e26ae2c735411292
dmong <dt2980_hee@naver.com>
2024-04-05 04:30:03 +0900

    Feat: 존재하지 않는 엔티티 조회 시 응답할 `BadRequestException` 구현

---
+++ backend/grabtable/src/main/java/edu/skku/grabtable/exception/BadRequestException.java
@@ -0,0 +1,15 @@
+package edu.skku.grabtable.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BadRequestException extends RuntimeException {
+
+    private final int code;
+    private final String message;
+
+    public BadRequestException(ExceptionCode exceptionCode) {
+        this.code = exceptionCode.getCode();
+        this.message = exceptionCode.getMessage();
+    }
+}

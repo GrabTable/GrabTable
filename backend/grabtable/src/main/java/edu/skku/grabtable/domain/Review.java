d110ec7937767446588d82bc94c7b4590694180a
dmong <dt2980_hee@naver.com>
2024-04-05 04:20:43 +0900

    Feat: `Review` soft delete 구현

--- backend/grabtable/src/main/java/edu/skku/grabtable/domain/Review.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/domain/Review.java
@@ -1,5 +1,6 @@
 package edu.skku.grabtable.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -8,10 +9,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"reviewPlatform", "message", "rating", "status"})
+@SQLDelete(sql = "UPDATE review SET status = 'INVALID' WHERE id = ?")
+@SQLRestriction("status = 'VALID'")
 public class Review extends BaseTimeEntity {
 
     @Id

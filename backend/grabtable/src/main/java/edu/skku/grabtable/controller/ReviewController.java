08a155614084668c349cd735f8b4c364e37677ca
dmong <dt2980_hee@naver.com>
2024-04-05 04:35:15 +0900

    Feat: 리뷰 API 구현
    - 유저가 작성한 모든 리뷰 조회
    - 가게의 모든 리뷰 조회
    - 리뷰 등록
    - 리뷰 수정
    - 리뷰 삭제

--- backend/grabtable/src/main/java/edu/skku/grabtable/controller/ReviewController.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/controller/ReviewController.java
@@ -1,6 +1,17 @@
 package edu.skku.grabtable.controller;
 
+import edu.skku.grabtable.domain.request.ReviewRequest;
+import edu.skku.grabtable.domain.request.ReviewUpdateRequest;
+import edu.skku.grabtable.domain.response.ReviewResponse;
+import edu.skku.grabtable.service.ReviewService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +19,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
-    
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/users/{userId}")
+    public List<ReviewResponse> getAllReviewsByUser(@PathVariable Long userId) {
+        return reviewService.getAllReviewsByUser(userId);
+    }
+
+    @GetMapping("/stores/{storeId}")
+    public List<ReviewResponse> getAllReviewsByStore(@PathVariable Long storeId) {
+        return reviewService.getAllReviewsByStore(storeId);
+    }
+
+    //밑의 모든 엔드포인트는 AUTH 모듈과 통합 필요
+    @PostMapping
+    public void upload(@RequestBody ReviewRequest request) {
+        reviewService.upload(request);
+    }
+
+    @PatchMapping("/{reviewId}")
+    public void update(@RequestBody ReviewUpdateRequest request,
+                       @PathVariable Long reviewId) {
+        reviewService.update(reviewId, request.getMessage(), request.getRating());
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public void delete(@PathVariable Long reviewId) {
+        reviewService.delete(reviewId);
+    }
 }
--- backend/grabtable/src/main/java/edu/skku/grabtable/service/ReviewService.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/service/ReviewService.java
@@ -1,13 +1,23 @@
 package edu.skku.grabtable.service;
 
+import edu.skku.grabtable.domain.Review;
+import edu.skku.grabtable.domain.Store;
+import edu.skku.grabtable.domain.User;
+import edu.skku.grabtable.domain.request.ReviewRequest;
+import edu.skku.grabtable.domain.response.ReviewResponse;
+import edu.skku.grabtable.exception.BadRequestException;
+import edu.skku.grabtable.exception.ExceptionCode;
 import edu.skku.grabtable.repository.ReviewRepository;
 import edu.skku.grabtable.repository.StoreRepository;
 import edu.skku.grabtable.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReviewService {
 
     private final UserRepository userRepository;
@@ -15,4 +25,41 @@ public class ReviewService {
     private final StoreRepository storeRepository;
 
 
+    @Transactional(readOnly = true)
+    public List<ReviewResponse> getAllReviewsByUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BadRequestException(ExceptionCode.NOT_FOUND_USER_ID));
+
+        return user.getReviews().stream().map(ReviewResponse::of).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReviewResponse> getAllReviewsByStore(Long storeId) {
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new BadRequestException(ExceptionCode.NOT_FOUND_STORE_ID));
+
+        return store.getReviews().stream().map(ReviewResponse::of).toList();
+    }
+
+    public void delete(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new BadRequestException(ExceptionCode.NOT_FOUND_REVIEW_ID));
+        reviewRepository.delete(review);
+    }
+
+    public void update(Long reviewId, String message, Double rating) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new BadRequestException(ExceptionCode.NOT_FOUND_STORE_ID));
+        review.update(message, rating);
+    }
+
+    public void upload(ReviewRequest request) {
+        Store store = storeRepository.findById(request.getStoreId())
+                .orElseThrow(() -> new BadRequestException(ExceptionCode.NOT_FOUND_STORE_ID));
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new BadRequestException(ExceptionCode.NOT_FOUND_USER_ID));
+
+        Review review = Review.of(store, user, request.getMessage(), request.getRating());
+        reviewRepository.save(review);
+    }
 }

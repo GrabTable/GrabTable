8a09a23cd5d46a3e3e0470b3c141fef5bf292511
goo314 <rn0731@g.skku.edu>
2024-04-06 00:07:43 +0900

    Feat(fe): Create Footer

---
+++ frontend/grabtable/src/components/footer.tsx
@@ -0,0 +1,20 @@
+import { FaGithub } from 'react-icons/fa6'
+import { FaRegCopyright } from 'react-icons/fa6'
+
+export default function Footer() {
+  return (
+    <footer className="w-full h-24 bg-gray-light p-4 flex items-center">
+      <div className="w-full flex justify-end items-center gap-1">
+        <FaRegCopyright />
+        <p className="text-center text-medium font-bold">Grab Table</p>
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://github.com/GrabTable/GrabTable"
+        >
+          <FaGithub className="cursor-pointer hover:opacity-70" size={32} />
+        </a>
+      </div>
+    </footer>
+  )
+}

3473b4da544f6828edc0902e61bdb20eac8dec6a
dmong <dt2980_hee@naver.com>
2024-04-08 04:01:40 +0900

    Test(be): 리뷰 통합 테스트 추가

---
+++ backend/grabtable/src/test/java/edu/skku/grabtable/integration/ReviewIntegrationTest.java
@@ -0,0 +1,72 @@
+package edu.skku.grabtable.integration;
+
+import edu.skku.grabtable.domain.Review;
+import edu.skku.grabtable.domain.ReviewStatus;
+import edu.skku.grabtable.domain.Store;
+import edu.skku.grabtable.domain.User;
+import edu.skku.grabtable.domain.request.ReviewRequest;
+import edu.skku.grabtable.domain.response.ReviewResponse;
+import edu.skku.grabtable.repository.ReviewRepository;
+import edu.skku.grabtable.repository.StoreRepository;
+import edu.skku.grabtable.repository.UserRepository;
+import edu.skku.grabtable.service.ReviewService;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@Sql(value = {
+        "classpath:data/stores.sql",
+        "classpath:data/reviews.sql"
+})
+public class ReviewIntegrationTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    StoreRepository storeRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Autowired
+    ReviewService reviewService;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("리뷰 삭제 시 상태가 INVALID로 변경된다. (SOFT DELETE)")
+    void uploadReview() {
+        //given
+        User user = User.builder()
+                .username("userA")
+                .password("1234")
+                .email("userA@gmail.com")
+                .phone("01012345678")
+                .build();
+
+        User savedUser = userRepository.save(user);
+        User findUser = userRepository.findById(savedUser.getId()).get();
+        Store store = storeRepository.findByStoreName("봉수육").get();
+        reviewService.upload(new ReviewRequest(findUser.getId(), store.getId(), "맛있어요", 4.0));
+        ReviewResponse reviewResponse = reviewService.getAllReviewsByUser(findUser.getId()).get(0);
+
+        //when
+        reviewService.delete(reviewResponse.getId());
+
+        //then
+
+        //NativeQuery를 통해 JPA 설정을 우회할 수 있다.
+        Review deletedReview = (Review) em.createNativeQuery("select * from review where status = 'INVALID'",
+                        Review.class)
+                .getResultList().get(0);
+
+        Assertions.assertThat(deletedReview.getStatus()).isEqualTo(ReviewStatus.INVALID);
+    }
+}

6cf998d68dfbb610012d423c59adfce2cbcdc070
dmong <dt2980_hee@naver.com>
2024-04-08 03:55:47 +0900

    Fix(be): 테스트 시드 데이터 오류 수정

--- backend/grabtable/src/test/resources/data/stores.sql
+++ backend/grabtable/src/test/resources/data/stores.sql
@@ -1,10 +1,12 @@
+INSERT INTO store (id, created_at, last_modified_at, address, description, phone, store_name, store_picture_url,
+                   category, status)
+VALUES (1, NULL, NULL, NULL, NULL, NULL, '봉수육', NULL, 'KOREAN', 'VALID');
 
-INSERT INTO STORE (id, created_at, last_modified_at, address, description, phone, store_name, store_picture_url, category, status)
-VALUES (1, NULL, NULL, NULL, NULL, '봉수육', NULL, 'KOREAN', 'VALID');
-                
-INSERT INTO STORE (id, created_at, last_modified_at, address, description, phone, store_name, store_picture_url, category, status)
-VALUES (2, NULL, NULL, NULL, NULL, '모수밀면', NULL, 'KOREAN', 'VALID');
-        
-INSERT INTO STORE (id, created_at, last_modified_at, address, description, phone, store_name, store_picture_url, category, status)
-VALUES (3, NULL, NULL, NULL, NULL, '먹깨비김밥', NULL, 'KOREAN', 'VALID');
+INSERT INTO store (id, created_at, last_modified_at, address, description, phone, store_name, store_picture_url,
+                   category, status)
+VALUES (2, NULL, NULL, NULL, NULL, NULL, '모수밀면', NULL, 'KOREAN', 'VALID');
+
+INSERT INTO store (id, created_at, last_modified_at, address, description, phone, store_name, store_picture_url,
+                   category, status)
+VALUES (3, NULL, NULL, NULL, NULL, NULL, '먹깨비김밥', NULL, 'KOREAN', 'VALID');
         

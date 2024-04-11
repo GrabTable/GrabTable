6ce69547449c77e0cfb982330193a65dc51cd6e4
dmong <dt2980_hee@naver.com>
2024-04-12 08:44:39 +0900

    Refactor(be): JWT 생성, 정보 읽기, 검증 로직 분리

---
+++ backend/grabtable/src/main/java/edu/skku/grabtable/auth/JwtUtil.java
@@ -0,0 +1,89 @@
+package edu.skku.grabtable.auth;
+
+import edu.skku.grabtable.auth.domain.UserTokens;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+    private final Long accessTokenExpiry;
+    private final Long refreshTokenExpiry;
+
+    public JwtUtil(
+            @Value("${spring.auth.jwt.secret-key}") final String secretKey,
+            @Value("${spring.auth.jwt.access-token-expiry}") final Long accessTokenExpiry,
+            @Value("${spring.auth.jwt.refresh-token-expiry}") final Long refreshTokenExpiry
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.accessTokenExpiry = accessTokenExpiry;
+        this.refreshTokenExpiry = refreshTokenExpiry;
+    }
+
+    //===토큰 생성===//
+
+    public UserTokens createLoginToken(String subject) {
+        String refreshToken = createToken("", refreshTokenExpiry);
+        String accessToken = createToken(subject, accessTokenExpiry);
+        return new UserTokens(refreshToken, accessToken);
+    }
+
+    private String createToken(String subject, Long expiredMs) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String reissueAccessToken(String subject) {
+        return createToken(subject, accessTokenExpiry);
+    }
+
+    //===토큰 정보 추출===//
+
+    public String getSubject(String token) {
+        return parseToken(token)
+                .getBody().getSubject();
+    }
+
+    private Jws<Claims> parseToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token);
+    }
+
+    //===토큰 유효성 검사===//
+    //예외 처리 커스텀 TODO
+    public boolean validateRefreshToken(String refreshToken) {
+        try {
+            parseToken(refreshToken);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateAccessToken(String accessToken) {
+        try {
+            parseToken(accessToken);
+        } catch (ExpiredJwtException e) {
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+        return false;
+    }
+}

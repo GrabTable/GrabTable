9d98d3194b4549415e075b5ccdf36e2f2f4be05a
dmong <dt2980_hee@naver.com>
2024-04-12 06:26:39 +0900

    Chore(be): `RefreshToken` 생성자 lombok 적용

--- backend/grabtable/src/main/java/edu/skku/grabtable/auth/domain/RefreshToken.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/auth/domain/RefreshToken.java
@@ -3,20 +3,16 @@ package edu.skku.grabtable.auth.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshToken {
     @Id
     private Long userId;
-
     private String value;
-
-    public RefreshToken(Long userId, String value) {
-        this.userId = userId;
-        this.value = value;
-    }
 }

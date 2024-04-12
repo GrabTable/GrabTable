17862aa5fb4577136c16ed5baea26d63926213a9
cona <rn0731@g.skku.edu>
2024-04-12 20:38:15 +0900

    Feat(fe): prettier 세팅 (#19)
    * Feat(fe): install prettier
    
    * Chore(fe): run prettier write
    
    * Feat(fe): Add github workflow

--- frontend/grabtable/src/middleware.ts
+++ frontend/grabtable/src/middleware.ts
@@ -1,6 +1,4 @@
-
-
-// TODO :로그인 상태 관리 -> 안되어 있으면 리다이랙트 
-export default ( () => {
-    console.log("middle")
-})
+// TODO :로그인 상태 관리 -> 안되어 있으면 리다이랙트
+export default () => {
+  console.log('middle')
+}
--- frontend/grabtable/tailwind.config.ts
+++ frontend/grabtable/tailwind.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from "tailwindcss";
-import colors from 'tailwindcss/colors';
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         blue: '#4C4C6D',
@@ -20,8 +20,8 @@ const config: Config = {
         yellow: '#FBD36B',
         'gray-light': colors.neutral[100],
       },
-    }
+    },
   },
   plugins: [],
-};
-export default config;
+}
+export default config

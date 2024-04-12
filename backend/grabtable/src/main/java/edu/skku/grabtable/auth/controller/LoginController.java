2815842a4a70ad7faa95c436db1ee816280a24eb
dmong <dt2980_hee@naver.com>
2024-04-12 09:37:46 +0900

    Chore(be): 테스트 API가 카카오 인증 code를 출력하도록 수정

--- backend/grabtable/src/main/java/edu/skku/grabtable/auth/controller/LoginController.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/auth/controller/LoginController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -72,8 +73,8 @@ public class LoginController {
     }
 
     @GetMapping("/login/test")
-    public String test() {
-        log.info("redirected");
+    public String test(@RequestParam String code) {
+        log.info("code={}", code);
         return "ok";
     }
 }

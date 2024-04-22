39981ba2daa1e2aba37fed313c52a3bf73ca0a5b
dmong <dt2980_hee@naver.com>
2024-04-22 17:41:44 +0900

    Refactor(be): Store-Menu 매핑 양방향 설정
    DTO 반환 편의 고려

--- backend/grabtable/src/main/java/edu/skku/grabtable/store/domain/Menu.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/store/domain/Menu.java
@@ -1,16 +1,23 @@
-package edu.skku.grabtable.domain;
+package edu.skku.grabtable.store.domain;
 
+import edu.skku.grabtable.domain.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Menu extends BaseTimeEntity {
 
     @Id
@@ -18,6 +25,7 @@ public class Menu extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "store_id")
     private Store store;
 
     private String menuName;

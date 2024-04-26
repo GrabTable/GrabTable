007f3692a3c90a16239b98d092ed105084c4bf72
chanyeong <jcy030896@naver.com>
2024-04-26 15:40:16 +0900

    chore(be): verify의 응답을 bill response로 변경

--- backend/grabtable/src/main/java/edu/skku/grabtable/payment/controller/PaymentController.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/payment/controller/PaymentController.java
@@ -3,21 +3,26 @@ package edu.skku.grabtable.payment.controller;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import edu.skku.grabtable.payment.domain.Bill;
 import edu.skku.grabtable.payment.domain.request.PaymentRequestDto;
+import edu.skku.grabtable.payment.domain.response.BillResponse;
 import edu.skku.grabtable.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+
 @RequestMapping("/v1/payment")
 @RequiredArgsConstructor
 @RestController
 public class PaymentController {
 
     private final PaymentService paymentService;
+
     @PostMapping(value = "/verify")
-    public ResponseEntity<Bill> paymentByImpUid(@RequestBody PaymentRequestDto paymentDto) throws IamportResponseException, IOException {
+    public BillResponse paymentByImpUid(@RequestBody PaymentRequestDto paymentDto) throws IamportResponseException, IOException {
         Bill bill = paymentService.verify(paymentDto);
-        return ResponseEntity.ok(bill);
+        return BillResponse.of(bill);
     }
 }

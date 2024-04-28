d9613f847d3da6c76d7552296961e2e6c0fc1816
dmong <dt2980_hee@naver.com>
2024-04-29 02:55:29 +0900

    Feat(be): 주문 생성 API 구현

--- backend/grabtable/src/main/java/edu/skku/grabtable/order/controller/OrderController.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/order/controller/OrderController.java
@@ -1,2 +1,32 @@
-package edu.skku.grabtable.order.controller;public class OrderController {
+package edu.skku.grabtable.order.controller;
+
+import edu.skku.grabtable.auth.annotation.AuthUser;
+import edu.skku.grabtable.order.service.OrderService;
+import edu.skku.grabtable.order.domain.response.OrderResponse;
+import edu.skku.grabtable.user.domain.User;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public OrderResponse create(@AuthUser User user) {
+        return orderService.create(user);
+    }
+
+    @GetMapping
+    public List<OrderResponse> findAll() {
+
+    }
+
+    @GetMapping()
 }
--- backend/grabtable/src/main/java/edu/skku/grabtable/order/service/OrderService.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/order/service/OrderService.java
@@ -1,2 +1,47 @@
-package edu.skku.grabtable.order.service;public class OrderService {
+package edu.skku.grabtable.order.service;
+
+import edu.skku.grabtable.cart.domain.Cart;
+import edu.skku.grabtable.cart.domain.response.CartResponse;
+import edu.skku.grabtable.common.exception.BadRequestException;
+import edu.skku.grabtable.common.exception.ExceptionCode;
+import edu.skku.grabtable.order.domain.Order;
+import edu.skku.grabtable.order.domain.response.OrderResponse;
+import edu.skku.grabtable.order.repository.OrderRepository;
+import edu.skku.grabtable.reservation.domain.Reservation;
+import edu.skku.grabtable.reservation.repository.ReservationRepository;
+import edu.skku.grabtable.user.domain.User;
+import edu.skku.grabtable.user.repository.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final ReservationRepository reservationRepository;
+    private final UserRepository userRepository;
+
+    public OrderResponse create(User user) {
+        Reservation invitedReservation = user.getInvitedReservation();
+        if (invitedReservation != null) {
+            return buildOrderResponse(user, invitedReservation);
+        }
+
+        Reservation reservation = reservationRepository.findByHostId(user.getId())
+                .orElseThrow(() -> new BadRequestException(ExceptionCode.NO_RESERVATION_USER));
+
+        return buildOrderResponse(user, reservation);
+    }
+
+    private OrderResponse buildOrderResponse(User user, Reservation reservation) {
+        List<Cart> carts = user.getCarts();
+        Order order = new Order(reservation, carts);
+        orderRepository.save(order);
+        List<CartResponse> cartDTOs = carts.stream().map(CartResponse::of).toList();
+        return OrderResponse.of(order, user.getId(), cartDTOs);
+    }
 }

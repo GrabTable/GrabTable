2ef97dc2e1f5d0f5df8ca29fb4fd9408fdcbb3f9
dmong <dt2980_hee@naver.com>
2024-04-28 20:14:32 +0900

    Test(be): 예약 Service 테스트 추가

--- backend/grabtable/src/test/java/edu/skku/grabtable/reservation/service/ReservationServiceTest.java
+++ backend/grabtable/src/test/java/edu/skku/grabtable/reservation/service/ReservationServiceTest.java
@@ -1,12 +1,14 @@
 package edu.skku.grabtable.reservation.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import edu.skku.grabtable.common.exception.BadRequestException;
 import edu.skku.grabtable.domain.User;
 import edu.skku.grabtable.repository.UserRepository;
 import edu.skku.grabtable.reservation.domain.Reservation;
+import edu.skku.grabtable.reservation.domain.ReservationStatus;
 import edu.skku.grabtable.reservation.repository.ReservationRepository;
 import edu.skku.grabtable.store.domain.Store;
 import edu.skku.grabtable.store.domain.StoreCategory;
@@ -17,7 +19,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -41,48 +42,81 @@ class ReservationServiceTest {
 
     @Test
     @DisplayName("기존 예약의 호스트가 새로운 예약 생성 시 실패한다.")
-    void create_fail() {
+    void host_create_fail() {
         User user = new User(1L, "kakaoUser", "url", "userA", "1234", "email", "phone", null, new ArrayList<>());
         Store store = new Store(1L, "storeA", "Seoul", "url", "phone", "desc", StoreStatus.VALID,
                 StoreCategory.JAPANESE,
                 new ArrayList<>(), new ArrayList<>());
 
-        given(userRepository.findById(ArgumentMatchers.any()))
-                .willReturn(Optional.of(user));
-
-        given(storeRepository.findById(ArgumentMatchers.any()))
+        given(storeRepository.findById(any()))
                 .willReturn(Optional.of(store));
 
         //when
-        given(reservationRepository.existsByHostId(ArgumentMatchers.any()))
+        given(reservationRepository.existsByHostId(any()))
                 .willReturn(true);
 
         //then
-        assertThatThrownBy(() -> reservationService.createNewReservation(user.getId(), store.getId()))
-                .isInstanceOf(BadRequestException.class);
+        assertThatThrownBy(() -> reservationService.createNewReservation(user, store.getId()))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("이미 예약을 생성한 유저입니다.");
     }
 
     @Test
     @DisplayName("기존 예약의 호스트가 새로운 예약 참여 시 실패한다.")
-    void participate_fail() {
+    void host_participate_fail() {
         User user = new User(1L, "kakaoUser", "url", "userA", "1234", "email", "phone", null, new ArrayList<>());
         Store store = new Store(1L, "storeA", "Seoul", "url", "phone", "desc", StoreStatus.VALID,
                 StoreCategory.JAPANESE,
                 new ArrayList<>(), new ArrayList<>());
         Reservation reservation = new Reservation(user, store);
 
-        given(userRepository.findById(ArgumentMatchers.any()))
-                .willReturn(Optional.of(user));
-
-        given(reservationRepository.findByInviteCode(ArgumentMatchers.any()))
+        given(reservationRepository.findByInviteCode(any()))
                 .willReturn(Optional.of(reservation));
-
-        //when
-        given(reservationRepository.existsByHostId(ArgumentMatchers.any()))
+        given(reservationRepository.existsByHostId(any()))
                 .willReturn(true);
 
         //then
-        assertThatThrownBy(() -> reservationService.joinExistingReservation(user.getId(), reservation.getInviteCode()))
-                .isInstanceOf(BadRequestException.class);
+        assertThatThrownBy(() -> reservationService.joinExistingReservation(user, reservation.getInviteCode()))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("이미 예약을 생성한 유저입니다.");
+    }
+
+    @Test
+    @DisplayName("기존 예약의 참여자가 새로운 예약 생성 시 실패한다.")
+    void invitee_create_fail() {
+        Reservation reservation = new Reservation(1L, null, null, null, null, ReservationStatus.ONGOING);
+        User user = new User(1L, "kakaoUser", "url", "userA", "1234", "email", "phone", reservation, new ArrayList<>());
+        Store store = new Store(1L, "storeA", "Seoul", "url", "phone", "desc", StoreStatus.VALID,
+                StoreCategory.JAPANESE,
+                new ArrayList<>(), new ArrayList<>());
+
+        given(reservationRepository.existsByHostId(any()))
+                .willReturn(false);
+        given(storeRepository.findById(any()))
+                .willReturn(Optional.of(store));
+
+        assertThatThrownBy(() -> reservationService.createNewReservation(user, 1L))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("이미 예약에 초대된 유저입니다.");
+
+    }
+
+    @Test
+    @DisplayName("기존 예약의 참여자가 다른 예약에 참여 시도 시 실패한다")
+    void invitee_participate_fail() {
+        Reservation reservation = new Reservation(1L, null, null, null, null, ReservationStatus.ONGOING);
+        Reservation reservation2 = new Reservation(2L, null, null, null, null, ReservationStatus.ONGOING);
+        User user = new User(1L, "kakaoUser", "url", "userA", "1234", "email", "phone", reservation, new ArrayList<>());
+        Store store = new Store(1L, "storeA", "Seoul", "url", "phone", "desc", StoreStatus.VALID,
+                StoreCategory.JAPANESE,
+                new ArrayList<>(), new ArrayList<>());
+
+        given(reservationRepository.findByInviteCode(any()))
+                .willReturn(Optional.of(reservation2));
+
+        assertThatThrownBy(() -> reservationService.joinExistingReservation(user, "inviteCode"))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("이미 예약에 초대된 유저입니다.");
+
     }
 }

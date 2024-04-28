21fac8494ed6201441fc1293cc75b96188e60f80
dmong <dt2980_hee@naver.com>
2024-04-29 02:54:08 +0900

    Feat(be): Cart 응답 DTO 패키지 변경 및 정적 팩토리 메서드 추가

--- backend/grabtable/src/main/java/edu/skku/grabtable/reservation/domain/response/CartResponse.java
+++
@@ -1,7 +0,0 @@
-package edu.skku.grabtable.reservation.domain.response;
-
-public class CartResponse {
-    private String menuName;
-    private Integer quantity;
-    private Integer totalPrice;
-}

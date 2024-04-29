8f565d3d07690da48111c6f13af08d788d6e73a5
dmong <dt2980_hee@naver.com>
2024-04-29 19:11:58 +0900

    Test(be): Order API 테스트 작성

---
+++ backend/grabtable/src/test/java/edu/skku/grabtable/order/controller/OrderControllerTest.java
@@ -0,0 +1,59 @@
+package edu.skku.grabtable.order.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.skku.grabtable.common.ControllerTest;
+import edu.skku.grabtable.order.domain.response.OrderResponse;
+import edu.skku.grabtable.order.service.OrderService;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(OrderController.class)
+@AutoConfigureRestDocs
+class OrderControllerTest extends ControllerTest {
+
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String REFRESH_TOKEN = "refresh_token";
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    OrderService orderService;
+
+    @Test
+    @DisplayName("유저의 현재 카트를 기반으로 주문을 생성할 수 있다.")
+    void create() throws Exception {
+        //given
+        OrderResponse orderResponse = new OrderResponse(1L, 1L, null, "PENDING");
+        Mockito.when(orderService.create(any()))
+                .thenReturn(orderResponse);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/v1/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                        .cookie(new Cookie("refresh-token", REFRESH_TOKEN)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        OrderResponse actual = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                OrderResponse.class
+        );
+
+        assertThat(actual).usingRecursiveComparison().isEqualTo(orderResponse);
+    }
+}

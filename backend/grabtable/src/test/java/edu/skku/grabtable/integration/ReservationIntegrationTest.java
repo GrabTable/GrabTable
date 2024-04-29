1e2b8e5eedcfaa03c941df3eda50b969ba13b611
dmong <dt2980_hee@naver.com>
2024-04-30 01:26:04 +0900

    Test(be): 통합 테스트 템플릿 작성

---
+++ backend/grabtable/src/test/java/edu/skku/grabtable/integration/ReservationIntegrationTest.java
@@ -0,0 +1,33 @@
+package edu.skku.grabtable.integration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class ReservationIntegrationTest {
+
+    @Test
+    @DisplayName("호스트가 예약을 취소하면 예약의 모든 참여자의 예약 상태가 취소된다.")
+    void host_cancel_propagate() {
+    }
+
+    @Test
+    @DisplayName("참여자가 예약을 취소하면 예약의 참여자가 1명 줄어든 상태로 유지된다.")
+    void invitee_cancel_remain() {
+    }
+
+    @Test
+    @DisplayName("모든 참여자가 주문 완료 상태라면 예약 확정에 성공한다.")
+    void confirm_reservation() {
+    }
+
+    @Test
+    @DisplayName("유저 ID로 예약 찾을 시 현재 예약 정보를 확인할 수 있다.")
+    void findReservationDetailInfo() {
+        //TODO: ReservationService.findReservationByUser의 반환 응답이 미구현 상태
+    }
+
+}

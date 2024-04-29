ef22d9ed3b6b1a7b458cc958926a15236b451c10
dmong <dt2980_hee@naver.com>
2024-04-30 03:29:30 +0900

    Refactor(be): 카트 요청 DTO 분리

--- backend/grabtable/src/main/java/edu/skku/grabtable/cart/domain/request/CartUpdateRequest.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/cart/domain/request/CartUpdateRequest.java
@@ -3,9 +3,9 @@ package edu.skku.grabtable.cart.domain.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
-public class CartRequest {
-    Long menuId;
-    Integer quantity;
+@Getter
+public class CartUpdateRequest {
+    private Long cartId;
+    private Integer quantity;
 }

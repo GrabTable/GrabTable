21c12b51c4931b0bd55a3276f410741aed0db076
goo314 <rn0731@g.skku.edu>
2024-04-30 20:52:35 +0900

    Feat(fe): implement restaurant list page

---
+++ frontend/grabtable/src/components/RestaurantCard.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useRef } from "react";
+import { projectsData } from "@/lib/data";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { Badge } from "@/components/ui/badge"
+
+
+type Restaurant = {
+    address: string
+    averageRating: number
+    category: string
+    id: number
+    storeName: string
+    storePictureUrl: string
+}
+
+export default function Project({
+    id,
+    address,
+    averageRating,
+    category,
+    storeName,
+    storePictureUrl
+}: Restaurant) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.9, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scaleProgess,
+        opacity: opacityProgess,
+      }}
+      className="group my-4 sm:mb-8 last:mb-0"
+    >
+      <section className="bg-gray-100 border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative h-40 hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+          <h3 className="text-2xl font-semibold">{storeName}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+            {address}
+          </p>
+          <div className="mt-4">
+            <Badge>{category}</Badge>
+          </div>
+
+        </div>
+
+        <Image
+          src={storePictureUrl || '/Korean.jpeg'}
+          alt=""
+          width={100}
+          height={100}
+          quality={95}
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+        transition "
+        />
+      </section>
+    </motion.div>
+  );
+}

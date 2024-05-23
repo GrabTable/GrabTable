422a10c51095302b16af63d21713e72036f39f15
dmong <dt2980_hee@naver.com>
2024-05-23 12:15:46 +0900

    Feat(be): 테스트용 로그인 API 추가

--- backend/grabtable/src/main/java/edu/skku/grabtable/auth/controller/LoginController.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/auth/controller/LoginController.java
@@ -14,11 +14,11 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -68,9 +68,12 @@ public class LoginController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/login/test")
-    public String test(@RequestParam String code) {
-        log.info("code={}", code);
-        return "ok";
+    @GetMapping("/login/test/{testUserId}")
+    public ResponseEntity<UserTokens> test(
+            @PathVariable String testUserId,
+            HttpServletResponse response
+    ) {
+        UserTokens userTokens = loginService.testLogin("TestUser#" + testUserId, "TestUser#" + testUserId);
+        return ResponseEntity.ok(userTokens);
     }
 }
--- backend/grabtable/src/main/java/edu/skku/grabtable/cart/controller/CartController.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/cart/controller/CartController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("v1/carts")
+@RequestMapping("/v1/carts")
 @RestController
 @RequiredArgsConstructor
 public class CartController {

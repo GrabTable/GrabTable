ed8e5b4bde251b71915f5ef4f292f4877a39902b
gitFILO <x2xgudwls@g.skku.edu>
2024-06-07 22:03:47 +0900

    fix(fe): prettier 적용

--- frontend/grabtable/src/components/kakaoMap.tsx
+++ frontend/grabtable/src/components/kakaoMap.tsx
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
-import "react-kakao-maps-sdk";
+import { useEffect } from 'react'
+import 'react-kakao-maps-sdk'
 
 type Restaurant = {
-  address: string;
-  averageRating: number;
-  category: string;
-  id: number;
-  storeName: string;
-  storePictureUrl: string;
-  latitude: number;
-  longitude: number;
-};
+  address: string
+  averageRating: number
+  category: string
+  id: number
+  storeName: string
+  storePictureUrl: string
+  latitude: number
+  longitude: number
+}
 
 const KakaoMap = ({ stores }: { stores: Restaurant[] }) => {
   useEffect(() => {
-    const kakaoMapScript = document.createElement("script");
-    kakaoMapScript.async = false;
-    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
-    document.head.appendChild(kakaoMapScript);
+    const kakaoMapScript = document.createElement('script')
+    kakaoMapScript.async = false
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`
+    document.head.appendChild(kakaoMapScript)
 
-    const styleTag = document.createElement("style");
+    const styleTag = document.createElement('style')
     styleTag.innerHTML = `
       .wrap {position: absolute;left: 0;bottom: 40px;width: 288px;height: 132px;margin-left: -144px;text-align: left;overflow: hidden;font-size: 12px;font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;line-height: 1.5;}
       .wrap * {padding: 0;margin: 0;}
@@ -35,33 +35,33 @@ const KakaoMap = ({ stores }: { stores: Restaurant[] }) => {
       .info .img {position: absolute;top: 6px;left: 5px;width: 73px;height: 71px;border: 1px solid #ddd;color: #888;overflow: hidden;}
       .info:after {content: '';position: absolute;margin-left: -12px;left: 50%;bottom: 0;width: 22px;height: 12px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
       .info .link {color: #5085BB;}
-    `;
-    document.head.appendChild(styleTag);
+    `
+    document.head.appendChild(styleTag)
 
     const onLoadKakaoAPI = () => {
       window.kakao.maps.load(() => {
-        var container = document.getElementById("map");
-        if (!container) return;
+        var container = document.getElementById('map')
+        if (!container) return
         var options = {
           center: new window.kakao.maps.LatLng(37.2987099, 126.9713822),
           level: 4,
-        };
+        }
 
-        var map = new window.kakao.maps.Map(container, options);
+        var map = new window.kakao.maps.Map(container, options)
 
-        let markers: any[] = [];
-        let overlays: any[] = [];
+        let markers: any[] = []
+        let overlays: any[] = []
 
         stores.forEach((store) => {
           const markerPosition = new window.kakao.maps.LatLng(
             store.latitude,
-            store.longitude
-          );
+            store.longitude,
+          )
           const marker = new window.kakao.maps.Marker({
             position: markerPosition,
-          });
-          marker.setMap(map);
-          markers.push(marker);
+          })
+          marker.setMap(map)
+          markers.push(marker)
 
           const infowindowContent = `
             <div class="wrap">
@@ -81,36 +81,36 @@ const KakaoMap = ({ stores }: { stores: Restaurant[] }) => {
                   </div>
                 </div>
               </div>
-            </div>`;
+            </div>`
 
           const overlay = new window.kakao.maps.CustomOverlay({
             content: infowindowContent,
-            position: markerPosition
-          });
+            position: markerPosition,
+          })
 
-          overlays.push(overlay);
+          overlays.push(overlay)
 
-          window.kakao.maps.event.addListener(marker, "click", () => {
-            overlays.forEach(ov => ov.setMap(null));
-            overlay.setMap(map);
-          });
+          window.kakao.maps.event.addListener(marker, 'click', () => {
+            overlays.forEach((ov) => ov.setMap(null))
+            overlay.setMap(map)
+          })
 
           document.addEventListener('click', (e) => {
             if ((e.target as HTMLElement).matches('.close')) {
-              overlay.setMap(null);
+              overlay.setMap(null)
             }
-          });
-        });
+          })
+        })
 
         return () => {
-          markers.forEach((marker) => marker.setMap(null));
-          overlays.forEach((overlay) => overlay.setMap(null));
-        };
-      });
-    };
+          markers.forEach((marker) => marker.setMap(null))
+          overlays.forEach((overlay) => overlay.setMap(null))
+        }
+      })
+    }
 
-    kakaoMapScript.addEventListener("load", onLoadKakaoAPI);
-  }, [stores]);
+    kakaoMapScript.addEventListener('load', onLoadKakaoAPI)
+  }, [stores])
 
   return (
     <div>
@@ -119,7 +119,7 @@ const KakaoMap = ({ stores }: { stores: Restaurant[] }) => {
         <div id="clickLating"></div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default KakaoMap;
+export default KakaoMap

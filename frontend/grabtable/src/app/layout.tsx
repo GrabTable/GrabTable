143dd42bf490443d4cf27d816f20784521cf066c
dmong <dt2980_hee@naver.com>
2024-06-14 15:26:58 +0900

    Fix(fe): `Header.tsx`로 파일명 수정

--- frontend/grabtable/src/app/layout.tsx
+++ frontend/grabtable/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/Footer'
-import Header from '@/components/Header_temp'
+import Header from '@/components/Header'
 import ThemeSwitch from '@/components/Theme-switch'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import ThemeContextProvider from '@/context/theme-context'

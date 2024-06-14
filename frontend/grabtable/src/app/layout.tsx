870fd67e459b7d0760637d84fc44ce592d3dce01
dmong <dt2980_hee@naver.com>
2024-06-14 15:26:07 +0900

    Fix(fe): 대소문자 오류로 인한 임시파일명 수정

--- frontend/grabtable/src/app/layout.tsx
+++ frontend/grabtable/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/Footer'
-import Header from '@/components/Header'
+import Header from '@/components/Header_temp'
 import ThemeSwitch from '@/components/Theme-switch'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import ThemeContextProvider from '@/context/theme-context'

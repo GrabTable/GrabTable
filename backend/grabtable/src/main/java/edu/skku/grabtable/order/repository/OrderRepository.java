495f1bf4acd0be63f8f8c65e091aae170ef950e1
jinsu868 <mjinsu098@gmail.com>
2024-07-11 09:19:41 +0900

    refactor(be): Order 조회 시 user, carts fetch join 적용

--- backend/grabtable/src/main/java/edu/skku/grabtable/order/repository/OrderRepository.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/order/repository/OrderRepository.java
@@ -6,8 +6,17 @@ import edu.skku.grabtable.user.domain.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("""
+            SELECT o
+            FROM Order o
+            JOIN FETCH o.user u
+            JOIN FETCH o.carts c
+            WHERE  o.reservation = :reservation
+            """)
     List<Order> findByReservation(Reservation reservation);
 
     Optional<Order> findByReservationAndUser(Reservation reservation, User user);

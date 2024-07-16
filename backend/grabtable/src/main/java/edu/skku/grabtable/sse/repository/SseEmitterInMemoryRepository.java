c26cd33225eb63c9833c5324ba9887e9f380b570
dmong <dt2980_hee@naver.com>
2024-07-16 18:20:26 +0900

    Chore(be): 필요없는 어노테이션 제거

--- backend/grabtable/src/main/java/edu/skku/grabtable/sse/repository/SseEmitterInMemoryRepository.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/sse/repository/SseEmitterInMemoryRepository.java
@@ -2,14 +2,10 @@ package edu.skku.grabtable.sse.repository;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Repository
-@RequiredArgsConstructor
-@Transactional
 public class SseEmitterInMemoryRepository {
 
     private static final long timeout = 10L * 1000 * 60;

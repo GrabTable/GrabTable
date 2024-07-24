b9cf21699ceef75c381b5eb3595c7a0eb9d3d1ef
chanyeong <jcy030896@naver.com>
2024-07-24 13:06:12 +0900

    feat(be): redisson 적용 redisServer Config 작성

--- backend/grabtable/src/main/java/edu/skku/grabtable/common/config/RedisConfig.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/common/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package edu.skku.grabtable.common.config;
 
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,15 +19,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+    @Value("${spring.data.redis-pubsub.host}")
+    private String pubsubHost;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
+    @Value("${spring.data.redis-pubsub.port}")
+    private int pubsubPort;
+
+    @Value("${spring.data.redis-lock.host}")
+    private String lockHost;
+
+    @Value("${spring.data.redis-lock.port")
+    private int lockPort;
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory(pubsubHost, pubsubPort);
     }
 
     @Bean
@@ -41,5 +53,14 @@ public class RedisConfig {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(redisConnectionFactory);
         return container;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + lockHost + ":" + lockPort);
+        redisson = Redisson.create(config);
+        return redisson;
     }
 }

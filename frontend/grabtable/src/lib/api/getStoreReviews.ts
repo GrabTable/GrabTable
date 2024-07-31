b1a2d9c1e24a1e8ae326a87f70e6cca3296ca85c
dmong <dt2980_hee@naver.com>
2024-07-31 09:08:07 +0900

    Fix(fe): 리뷰 조회 API 페이징 기능

--- frontend/grabtable/src/lib/api/getStoreReviews.ts
+++ frontend/grabtable/src/lib/api/getStoreReviews.ts
@@ -1,6 +1,12 @@
 import { BASE_API_URL } from '../constants'
 
-export const getStoreReviews = async (storeId: string) => {
-  const res = await fetch(`${BASE_API_URL}/v1/reviews/stores/${storeId}`)
+export const getStoreReviews = async (
+  storeId: string,
+  cursor: number,
+  size: number,
+) => {
+  const res = await fetch(
+    `${BASE_API_URL}/v1/reviews/stores/${storeId}?cursor=${cursor}&size=${size}`,
+  )
   return res
 }

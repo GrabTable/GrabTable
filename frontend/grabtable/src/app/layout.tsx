32d0833759b851ccd61e5e35c7ce957e0191ea64
dmong <dt2980_hee@naver.com>
2024-08-04 03:41:49 +0900

    Chore(fe): React query 사용 코드 제거

--- frontend/grabtable/src/app/layout.tsx
+++ frontend/grabtable/src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Inter } from 'next/font/google'
 import { Suspense } from 'react'
 import { Toaster } from 'react-hot-toast'
 import './globals.css'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -27,13 +28,11 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
             <Suspense>{children}</Suspense>
             <Footer />
-
             <Toaster position="top-right" />
             <ThemeSwitch />
           </ActiveSectionContextProvider>

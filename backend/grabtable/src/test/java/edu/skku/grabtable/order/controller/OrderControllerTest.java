e1dddafb26c8f4767b5af9cf78d6262b05610fe1
chanyeong <jcy030896@naver.com>
2024-08-06 16:04:20 +0900

    fix: OrderControllerTest 복구

--- backend/grabtable/src/test/java/edu/skku/grabtable/order/controller/OrderControllerTest.java
+++ backend/grabtable/src/test/java/edu/skku/grabtable/order/controller/OrderControllerTest.java
@@ -1,12 +1,27 @@
 package edu.skku.grabtable.order.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.skku.grabtable.common.ControllerTest;
+import edu.skku.grabtable.order.domain.request.PaymentRequest;
+import edu.skku.grabtable.order.domain.response.OrderResponse;
 import edu.skku.grabtable.order.service.OrderService;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @WebMvcTest(OrderController.class)
 @AutoConfigureRestDocs
@@ -21,29 +36,29 @@ class OrderControllerTest extends ControllerTest {
     @MockBean
     OrderService orderService;
 
-//    @Test
-//    @DisplayName("유저의 현재 카트를 기반으로 주문을 생성할 수 있다.")
-//    void create() throws Exception {
-//        //given
-//        OrderResponse orderResponse = new OrderResponse(1L, 1L, null, 10000, "PAID");
-//        PaymentRequest paymentRequest = new PaymentRequest("1", 10000);
-//        Mockito.when(orderService.processPayment(any(), any()))
-//                .thenReturn(orderResponse);
-//
-//        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/v1/orders")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
-//                        .cookie(new Cookie("refresh-token", REFRESH_TOKEN))
-//                        .content(objectMapper.writeValueAsString(paymentRequest)))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(status().isCreated())
-//                .andReturn();
-//
-//        OrderResponse actual = objectMapper.readValue(
-//                mvcResult.getResponse().getContentAsString(),
-//                OrderResponse.class
-//        );
-//
-//        assertThat(actual).usingRecursiveComparison().isEqualTo(orderResponse);
-//    }
+    @Test
+    @DisplayName("유저의 현재 카트를 기반으로 주문을 생성할 수 있다.")
+    void create() throws Exception {
+        //given
+        OrderResponse orderResponse = new OrderResponse(1L, 1L, null, 10000, "PAID");
+        PaymentRequest paymentRequest = new PaymentRequest("1", 10000);
+        Mockito.when(orderService.processPayment(any(), any()))
+                .thenReturn(orderResponse);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/v1/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                        .cookie(new Cookie("refresh-token", REFRESH_TOKEN))
+                        .content(objectMapper.writeValueAsString(paymentRequest)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        OrderResponse actual = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                OrderResponse.class
+        );
+
+        assertThat(actual).usingRecursiveComparison().isEqualTo(orderResponse);
+    }
 }

6f7888800cbaf19e11cafb2201b1fa6917c9ccc4
dmong <dt2980_hee@naver.com>
2024-08-20 09:47:50 +0900

    Feat(be): `CartRepository` 배치 업데이트 메소드 추가

--- backend/grabtable/src/main/java/edu/skku/grabtable/cart/repository/CartRepository.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/cart/repository/CartRepository.java
@@ -1,8 +1,10 @@
 package edu.skku.grabtable.cart.repository;
 
 import edu.skku.grabtable.cart.domain.Cart;
+import edu.skku.grabtable.user.domain.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
@@ -22,6 +24,14 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
             where r.id = :reservationId
             """)
     List<Cart> findHostCurrentCartsByReservationId(Long reservationId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Cart c set c.user = null where c.user = :user")
+    void clearByUser(User user);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Cart c set c.user = null where c.user in :users")
+    void clearByUsers(List<User> users);
 
 
 }

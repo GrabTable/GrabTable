f755e0b8c69743dec7846981cd83fe4e4f58f702
dmong <dt2980_hee@naver.com>
2024-08-29 02:05:10 +0900

    Feat(be): 카프카 프로듀서 설정 초기화

---
+++ backend/grabtable/src/main/java/edu/skku/grabtable/common/config/KafkaProducerConfig.java
@@ -0,0 +1,43 @@
+package edu.skku.grabtable.common.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaProducerConfig {
+
+
+    private final String kafkaBootstrapUri;
+
+    public KafkaProducerConfig(
+            @Value("${spring.kafka.producer.bootstrap-servers}") String kafkaBootstrapUri
+    ) {
+        this.kafkaBootstrapUri = kafkaBootstrapUri;
+    }
+
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapUri);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(this.producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(this.producerFactory());
+    }
+}

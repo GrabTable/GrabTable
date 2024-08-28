ab132be737b7b4d803cf36a74005d6900e9b21d7
dmong <dt2980_hee@naver.com>
2024-08-29 02:05:32 +0900

    Feat(be): 예약 히스토리 이벤트 발생 시 카프카 메시지 전송

--- backend/grabtable/src/main/java/edu/skku/grabtable/reservation/service/ReservationEventHandler.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/reservation/service/ReservationEventHandler.java
@@ -1,18 +1,34 @@
 package edu.skku.grabtable.reservation.service;
 
+import edu.skku.grabtable.reservation.domain.ReservationHistory;
 import edu.skku.grabtable.reservation.domain.event.ReservationFinishEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ReservationEventHandler {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    @TransactionalEventListener
+    @Value("${topic.name}")
+    private String topicName;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    void sendReservationHistory(ReservationHistory reservationHistory) {
+        log.info("카프카 메시지 전송: {}", reservationHistory);
+        kafkaTemplate.send(topicName, reservationHistory);
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     void handleFinishEvent(ReservationFinishEvent event) {
         redisTemplate.convertAndSend(getChannelName(event.getReservationId()), event);
     }

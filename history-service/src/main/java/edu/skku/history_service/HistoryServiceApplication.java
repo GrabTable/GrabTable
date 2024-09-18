184b9b7eff184c18ebc837477a7b6892585ddaf0
dmong <dt2980_hee@naver.com>
2024-09-19 07:43:34 +0900

    Init(be): 히스토리 서비스 (컨슈머단) 초기화

---
+++ history-service/src/main/java/edu/skku/history_service/HistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.skku.history_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryServiceApplication.class, args);
+	}
+
+}
---
+++ history-service/src/test/java/edu/skku/history_service/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.skku.history_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

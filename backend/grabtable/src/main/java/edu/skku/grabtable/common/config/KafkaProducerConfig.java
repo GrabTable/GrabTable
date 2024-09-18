5bbe52e85c1a1d9a84eb77a2b2e322f612bbc495
dmong <dt2980_hee@naver.com>
2024-09-19 07:38:25 +0900

    Feat(be): 멱등성 프로듀서 설정 추가

--- backend/grabtable/src/main/java/edu/skku/grabtable/common/config/KafkaProducerConfig.java
+++ backend/grabtable/src/main/java/edu/skku/grabtable/common/config/KafkaProducerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -27,8 +28,9 @@ public class KafkaProducerConfig {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapUri);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return props;
     }
 
